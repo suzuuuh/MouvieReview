f358935b40572efcf70bce6dc9808f1b74def8a6
EC2 Default User <ec2-user@ip-172-31-4-28.ap-northeast-1.compute.internal>
2023-06-12 12:56:47 +0000

    レビュー削除

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,8 +1,11 @@
 class Admin::MoviesController < ApplicationController
+  before_action :authenticate_admin!
   def index
+    @movies = Movie.all
   end
 
   def show
+    @movie = Movie.find(params[:id])
   end
 
   def new
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
   def show
   end
 
@@ -7,4 +8,11 @@ class Admin::PostsController < ApplicationController
 
   def index
   end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_root_path
+  end
+
 end
