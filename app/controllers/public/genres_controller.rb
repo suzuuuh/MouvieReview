3a6515523d57407b2a57be7420c1c422e3694a97
EC2 Default User <ec2-user@ip-172-31-4-28.ap-northeast-1.compute.internal>
2023-06-24 04:21:10 +0000

    search

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -2,5 +2,9 @@ class Public::GenresController < ApplicationController
   def show
     @genres = Genre.all
     @genre = Genre.find(params[:id])
+    @message = ""
+    if @genre.empty?
+      @message = "該当作品なし"
+    end
   end
 end
